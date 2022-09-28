3e9457a248abecf0d336b465d77d5a6c2cb53e56
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-28 10:28:26 +0530

    Add project files.

---
+++ StackAList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StackAList
+    {
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stack<string> stack= new Stack<string>();
+            stack.Push("Sham");
+            stack.Push("Ram");
+            stack.Push("Shivam");
+            stack.Push("Amol");
+            stack.Pop();
+            stack.Pop();
+
+            stack.Pop();
+            stack.Peek();
+            stack.Push("Rani");
+
+            foreach (var l in stack)
+            {
+                Console.WriteLine(l);
+            }
+            string[] names = new string[1];
+
+            
+
+
+
+        }
+
+    }
+
+
+}
